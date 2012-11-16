2fdbe45d3b11a2059066eb2bae389e410a019ea9
chinmay <cdbapat92@gmail.com>
2012-11-16 08:07:12 +0530

    Added indentation

--- print.pl
+++ print.pl
@@ -4,7 +4,8 @@ show(_, _, Y):-
 show(State, X, Y):-
     X > 9,
     Yn is Y - 1,
-    write('\n'),
+    write('\n\n'),
+    boardIndent(Y),
     show(State, 1, Yn).
 
 
@@ -26,3 +27,15 @@ show([White, Black], X, Y):-
     write(' b-'), write( Piece),write('  '), 
     Xn is X + 1,
     show([White,Black], Xn, Y).
+
+
+boardIndent(Y):-
+    Y is 9,
+    write('     ').
+
+
+boardIndent(Y):-
+    write('     '),
+    Ytmp is Y + 1,
+    boardIndent(Ytmp).
+
